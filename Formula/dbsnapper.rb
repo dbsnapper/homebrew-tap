9e679891d26dad8ecb43b9af4af175f185aac06b
releaserbot <contact@dbsnapper.com>
2024-02-19 14:22:06 -0700

    Brew formula update for dbsnapper version v2.0.0-alpha.1

--- Formula/dbsnapper.rb
+++ Formula/dbsnapper.rb
@@ -9,7 +9,7 @@ class Dbsnapper < Formula
   depends_on :macos
 
   url "https://github.com/dbsnapper/dbsnapper/releases/download/v2.0.0-alpha.1/dbsnapper_Darwin_all.zip"
-  sha256 "a843c57d89a2fb527c5fa4d8151aafd8ba9fe7234b313f8117719ba63ecdd583"
+  sha256 "97802dcd08afc2f1258b6e5aeaefaf2b7c92125150b60ca23d5c5bed4475a9a0"
 
   def install
     bin.install "dbsnapper"
