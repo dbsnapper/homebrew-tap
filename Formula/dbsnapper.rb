f64026fcd6c652b4eed4a9bcb28497a97f979f5f
releaserbot <contact@dbsnapper.com>
2024-05-14 18:59:23 +0200

    Brew formula update for dbsnapper version v2.4.1

--- Formula/dbsnapper.rb
+++ Formula/dbsnapper.rb
@@ -5,11 +5,11 @@
 class Dbsnapper < Formula
   desc "Database Snapshotting, Subsetting, Sanitization, and Sharing made easy"
   homepage "https://dbsnapper.com/"
-  version "2.4.0"
+  version "2.4.1"
   depends_on :macos
 
-  url "https://github.com/dbsnapper/dbsnapper/releases/download/v2.4.0/dbsnapper_Darwin_all.zip"
-  sha256 "05be84394b358dc704e228abb8b0bc9434d4fb4d632175ca4d4312706ad07d38"
+  url "https://github.com/dbsnapper/dbsnapper/releases/download/v2.4.1/dbsnapper_Darwin_all.zip"
+  sha256 "de63f1f4a263c1b9849aa8fc9d47a7d11d8da21f791770019d186325ddf59aea"
 
   def install
     bin.install "dbsnapper"
